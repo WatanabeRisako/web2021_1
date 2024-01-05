4062f306acbe028e046ee5d68579cf020763152d
WatanabeRisako <132030325+WatanabeRisako@users.noreply.github.com>
2024-01-05 10:42:35 +0000

    とりあえず

--- home.js
+++ home.js
@@ -1,20 +1,10 @@
-  var $some = $('.some');
-  var original = $some.html();
-  
-  $('.all').change(function(){
-    var val1 = $(this).val();
-
-    $some.html(original).find('option'').each(function(){
-      var val2 = $(this).data('val');
-      
-      if(val1 != val2){
-        $(this).not(':first-some').remove();
-  }
-    });
-    
-    if($(this).val() === ''){
-      $children.attr('disabled', 'disabled');
-    }else{
-      $some.removeAttr('disabled');
-    }
-  });
+// app.get("/db",(req,res)=>{
+//   db.serialize(()=> {
+//     db.all("select TotK.id,name from TotK;",(error,data)=>{
+//       if(error){
+//         res.render('show',{mes:"エラーです"});
+//       }
+//       res.render('home',{data:data});
+//     })
+//   })
+// });
--- index.js
+++ index.js
@@ -28,7 +28,225 @@ app.get("/home", (req, res) => {
     })
 })
 
+app.get("/data_1", (req, res) => {
+    db.serialize( () => {
+        db.all("select TotK.id ,name from TotK where data_id=1;", (error, row) => {
+            if( error ) {
+                res.render('show', {mes:"エラーです"});
+            }
+            res.render('home', {data:row});
+        })
+    })
+})
 
+app.get("/data_2", (req, res) => {
+    db.serialize( () => {
+        db.all("select TotK.id ,name from TotK where data_id=2;", (error, row) => {
+            if( error ) {
+                res.render('show', {mes:"エラーです"});
+            }
+            res.render('home', {data:row});
+        })
+    })
+})
+
+app.get("/data_3", (req, res) => {
+    db.serialize( () => {
+        db.all("select TotK.id ,name from TotK where data_id=3;", (error, row) => {
+            if( error ) {
+                res.render('show', {mes:"エラーです"});
+            }
+            res.render('home', {data:row});
+        })
+    })
+})
+
+app.get("/data_4", (req, res) => {
+    db.serialize( () => {
+        db.all("select TotK.id ,name from TotK where data_id=4;", (error, row) => {
+            if( error ) {
+                res.render('show', {mes:"エラーです"});
+            }
+            res.render('home', {data:row});
+        })
+    })
+})
+
+app.get("/map_1", (req, res) => {
+    db.serialize( () => {
+        db.all("select TotK.id ,name from TotK where map_id=1;", (error, row) => {
+            if( error ) {
+                res.render('show', {mes:"エラーです"});
+            }
+            res.render('home', {data:row});
+        })
+    })
+})
+
+app.get("/map_2", (req, res) => {
+    db.serialize( () => {
+        db.all("select TotK.id ,name from TotK where map_id=2;", (error, row) => {
+            if( error ) {
+                res.render('show', {mes:"エラーです"});
+            }
+            res.render('home', {data:row});
+        })
+    })
+})
+
+app.get("/map2_1", (req, res) => {
+    db.serialize( () => {
+        db.all("select TotK.id ,name from TotK where map2_id=1;", (error, row) => {
+            if( error ) {
+                res.render('show', {mes:"エラーです"});
+            }
+            res.render('home', {data:row});
+        })
+    })
+})
+
+app.get("/map2_2", (req, res) => {
+    db.serialize( () => {
+        db.all("select TotK.id ,name from TotK where map2_id=2;", (error, row) => {
+            if( error ) {
+                res.render('show', {mes:"エラーです"});
+            }
+            res.render('home', {data:row});
+        })
+    })
+})
+
+app.get("/map2_3", (req, res) => {
+    db.serialize( () => {
+        db.all("select TotK.id ,name from TotK where map2_id=3;", (error, row) => {
+            if( error ) {
+                res.render('show', {mes:"エラーです"});
+            }
+            res.render('home', {data:row});
+        })
+    })
+})
+
+app.get("/map2_4", (req, res) => {
+    db.serialize( () => {
+        db.all("select TotK.id ,name from TotK where map2_id=4;", (error, row) => {
+            if( error ) {
+                res.render('show', {mes:"エラーです"});
+            }
+            res.render('home', {data:row});
+        })
+    })
+})
+
+app.get("/map2_5", (req, res) => {
+    db.serialize( () => {
+        db.all("select TotK.id ,name from TotK where map2_id=5;", (error, row) => {
+            if( error ) {
+                res.render('show', {mes:"エラーです"});
+            }
+            res.render('home', {data:row});
+        })
+    })
+})
+
+app.get("/map2_6", (req, res) => {
+    db.serialize( () => {
+        db.all("select TotK.id ,name from TotK where map2_id=6;", (error, row) => {
+            if( error ) {
+                res.render('show', {mes:"エラーです"});
+            }
+            res.render('home', {data:row});
+        })
+    })
+})
+
+app.get("/map2_7", (req, res) => {
+    db.serialize( () => {
+        db.all("select TotK.id ,name from TotK where map2_id=7;", (error, row) => {
+            if( error ) {
+                res.render('show', {mes:"エラーです"});
+            }
+            res.render('home', {data:row});
+        })
+    })
+})
+
+app.get("/map2_8", (req, res) => {
+    db.serialize( () => {
+        db.all("select TotK.id ,name from TotK where map2_id=8;", (error, row) => {
+            if( error ) {
+                res.render('show', {mes:"エラーです"});
+            }
+            res.render('home', {data:row});
+        })
+    })
+})
+
+app.get("/treasure_1", (req, res) => {
+    db.serialize( () => {
+        db.all("select TotK.id ,name from TotK where treasure_id=1;", (error, row) => {
+            if( error ) {
+                res.render('show', {mes:"エラーです"});
+            }
+            res.render('home', {data:row});
+        })
+    })
+})
+
+app.get("/treasure_2", (req, res) => {
+    db.serialize( () => {
+        db.all("select TotK.id ,name from TotK where treasure_id=2;", (error, row) => {
+            if( error ) {
+                res.render('show', {mes:"エラーです"});
+            }
+            res.render('home', {data:row});
+        })
+    })
+})
+
+app.get("/treasure_3", (req, res) => {
+    db.serialize( () => {
+        db.all("select TotK.id ,name from TotK where treasure_id=3;", (error, row) => {
+            if( error ) {
+                res.render('show', {mes:"エラーです"});
+            }
+            res.render('home', {data:row});
+        })
+    })
+})
+
+app.get("/treasure_4", (req, res) => {
+    db.serialize( () => {
+        db.all("select TotK.id ,name from TotK where treasure_id=4;", (error, row) => {
+            if( error ) {
+                res.render('show', {mes:"エラーです"});
+            }
+            res.render('home', {data:row});
+        })
+    })
+})
+
+app.get("/treasure_5", (req, res) => {
+    db.serialize( () => {
+        db.all("select TotK.id ,name from TotK where treasure_id=5;", (error, row) => {
+            if( error ) {
+                res.render('show', {mes:"エラーです"});
+            }
+            res.render('home', {data:row});
+        })
+    })
+})
+
+app.get("/treasure_6", (req, res) => {
+    db.serialize( () => {
+        db.all("select TotK.id ,name from TotK where treasure_id=6;", (error, row) => {
+            if( error ) {
+                res.render('show', {mes:"エラーです"});
+            }
+            res.render('home', {data:row});
+        })
+    })
+})
 
 app.get("/detail/:id", (req, res) => {
   let sql= "select Data.data_name as dname, Map.map_name as mname, Map2.map2_name as m2name,Treasure.treasure_name as tname from TotK inner join Data on TotK.data_id = data.id join Map on TotK.map_id = Map.id join Map2 on TotK.map2_id = Map2.id join Treasure on TotK.treasure_id = Treasure.id where Totk.id =" + req.params.id + ";"
@@ -74,9 +292,8 @@ app.post("/insert",(req,res) =>{
 
 app.get("/top", (req, res) => {
     //console.log(req.query.pop);    // ①
-    let desc = "";
+    let desc = "select id, 都道府県, 人口 from example order by 人口" + desc + " limit " + req.query.pop +";";
     if( req.query.desc ) desc = " desc";
-    let sql = "select id, 都道府県, 人口 from example order by 人口" + desc + " limit " + req.query.pop + ";";
     //console.log(sql);    // ②
     db.serialize( () => {
         db.all(sql, (error, data) => {
@@ -88,6 +305,7 @@ app.get("/top", (req, res) => {
         })
     })
 })
+
 app.use(function(req, res, next) {
   res.status(404).send('ページが見つかりません');
 });
